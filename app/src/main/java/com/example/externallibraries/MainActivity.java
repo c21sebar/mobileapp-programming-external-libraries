012d07a3a06f626b9a9ab0f88e52888ecf0f2c1e
Sebastian Arneson <c21sebar@student.his.se>
2022-05-16 09:50:17 +0200

    Open an image of mount everest from wikipedia into the image view with glide external library.

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        Glide.with(this).load("https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Everest_kalapatthar_crop.jpg/256px-Everest_kalapatthar_crop.jpg").into(imageView);
     }
 }
